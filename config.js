0be5fcc4214ecdcc7744ab087dec62ce0eb56f4e
koobc <darekroman@gmail.com>
2020-03-27 21:34:07 +0100

    config

--- config.js
+++ config.js
@@ -2,4 +2,6 @@ module.exports = {
   db: 'mongodb+srv://admin:wFr6oVXf728FiYQ0@cluster0-8kivt.mongodb.net/test?retryWrites=true&w=majority',
   keySession: ['TWOJKLUCZ'], /* secret keys */
   maxAgeSession: 24 * 60 * 60 * 1000 //24 hours
+
+  ///dsfs
 }
